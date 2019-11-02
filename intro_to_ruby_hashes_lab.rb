478db6c6052d87fefd7259b99181d2bc52c9ccaf
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-02 22:32:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,6 @@ def monopoly_with_second_tier
     monopoly = {
       railroads: {
       pieces, 4
-        
       }
     }
   # Remember implicit return! We're returning this new Hash _without_ the
@@ -20,6 +19,11 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
+  
+  
+  
+  
+  
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
