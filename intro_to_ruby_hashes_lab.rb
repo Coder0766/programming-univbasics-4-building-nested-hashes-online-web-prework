e1d76e489256ecfa2e89388af8cd2f0a63521db7
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-02 22:35:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ end
 def monopoly_with_second_tier
     monopoly = {
       railroads: {
-      pieces, 4
+      :pieces => 4
       }
     } 
   # Remember implicit return! We're returning this new Hash _without_ the
