93fc7d8c0765ee6b53e46ec8273ff9621a72965d
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-03 01:28:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
       :one_piece_owned => 25, 
       :two_piece_owned => 50, 
       :three_piece_owned => 100, 
-      hash.new
+      :four_piece_owned => 200
       }
     } 
   }
