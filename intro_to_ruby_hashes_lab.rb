1539f64a1646b00dd750c3358b39f16dced680e6
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-03 01:29:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ end
 def monopoly_with_third_tier
   monopoly = {
       railroads: {
-      :rent_in_dollars {
+      :rent_in_dollars =>
       :one_piece_owned => 25, 
       :two_piece_owned => 50, 
       :three_piece_owned => 100, 
