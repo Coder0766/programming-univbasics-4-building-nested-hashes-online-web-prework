d8aa8341c9be8387f2961385e3a57206fa998c64
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-03 01:59:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,9 +22,9 @@ def monopoly_with_third_tier
   monopoly = {
       railroads: {
       names:{
-      :reading_railroad => ({})
-      :pennsylvania_railroad => ({})
-      :b_and_o_railroad => ({})
+      :reading_railroad => ({}),
+      :pennsylvania_railroad => ({}),
+      :b_and_o_railroad => ({}),
       :shortline_railroad => ({})
       }
       rent_in_dollars: {
