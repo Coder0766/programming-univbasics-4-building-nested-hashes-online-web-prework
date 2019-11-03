32ee59a83193d3555c31f1193135815c51971b0c
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-03 01:00:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,6 @@ def monopoly_with_third_tier
       rent_in_dollars = 4 
           }
         }
-      }
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
